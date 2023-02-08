4a15f9d9096051e0c1248a4603b57667d5fcbea0
Pranav Waghmare <piyushw55@gmail.com>
2023-02-09 01:40:01 +0530

    perfect number application update

---
+++ LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PerfectNumber
+    {
+        public void PerfectNo(int number)
+        {
+
+            int sum = 0, n;
+            
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n\tIt is a perfect number"); 
+            }
+            else
+            {
+                Console.WriteLine("\n\tIt is a perfect number");               
+            }
+        }
+    }
+    
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -4,25 +4,67 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to logical programs");
-            //Taking user input
-            Console.WriteLine("Please enter option");
-            Console.WriteLine("1:Fibonacci Series\n2:Prime Number\n3:Reverse Number");
-            int option = Convert.ToInt32(Console.ReadLine());
-            //Switch case to call different method 
-            switch (option)
+            char opt = 'Y';
+            while ((opt == 'y') || (opt == 'Y'))
             {
-                case 1:
-                    Console.WriteLine("Please enter number");
-                    int count = Convert.ToInt32(Console.ReadLine());
-                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
-                    fibonacciSeries.Fibonacci(count);
-                    break;
+                    Console.Clear();
+                    Console.Write("\n********************************************************\n");
+                    Console.Write("WEL-COME TO LOGICAL PROGRAM APPLICATION\n");
+                    Console.Write("********************************************************\n");
+                    //Taking user input
+                    Console.Write("\t-------------------\n");
+                    Console.Write("\tOPTIONS\n");
+                    Console.Write("\t-------------------\n");
+                    Console.WriteLine("\t1:Fibonacci Series\n\t2:Perfect Number\n\t3:Reverse Number\n\t4:Reverse Number\n\t5:Coupon Number\n\t6:StopWatch");
+                    Console.Write("\n_______________________________________\n");
+                    Console.Write("Your choice : ");
+                    short option = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("_______________________________________\n");
+                    int number, output = 0;
+                    //Switch case to call different method 
+                    switch (option)
+                    {
+                        case 1:
+                            Console.Write("=========================================\n");
+                            Console.Write("\tFibonacci Series Application\n");
+                            Console.Write("=========================================\n");
+                            Console.WriteLine("Please enter number");
+                            int count = Convert.ToInt32(Console.ReadLine());
+                            FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                            fibonacciSeries.Fibonacci(count);
+                            Console.Write("\n==================END====================\n");
+                            Console.Write("Enter any key to Exit.......");
+                            Console.ReadKey();
+                            Console.Write("\n\n\tDo you want to Continue With Application(Y/N) : ");
+                            opt = Console.ReadKey().KeyChar;
+                            break;
+                        case 2:
+                            Console.Write("=========================================\n");
+                            Console.Write("\tPerfect Number Application\n");
+                            Console.Write("=========================================\n");
+                            PerfectNumber perfect = new PerfectNumber();
+                            Console.Write("enter the Number : ");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            perfect.PerfectNo(number);
+                            Console.Write("\n==================END====================\n");
+                            Console.Write("Enter any key to Exit.......");
+                            Console.ReadKey();
+                            Console.Write("\n\n\tDo you want to Continue With Application(Y/N) : ");
+                            opt = Console.ReadKey().KeyChar;
+                            break;
+                        
 
-                default:
-                    Console.WriteLine("Please enter right option");
-                    break;
+                        default:
+                            Console.WriteLine("Please enter right option");
+                            Console.Write("Enter any key to Exit.......");
+                            Console.ReadKey();
+                            break;
+                    }
+               
             }
-        }
+                Console.Clear();
+                Console.Write("\n\n\tThank You For Using Application.......");
+                Console.ReadKey();
+          }
     }
 }
